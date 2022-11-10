07e0f1baf1d1adb89d813b26c4d2d701f70566b1
SaurinSavla <saurin.savla@gmail.com>
2022-11-10 17:53:38 +0530

    sec 4 vid 2

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,12 +1,66 @@
-const chalk = require('chalk')
+const chalk = require('chalk') // helps coloring text
+const yargs = require('yargs') //helps taking input
 const getNotes = require('./notes.js')
+//we install many packages because node js doesnt allow too many argument parsing
+
+//Customizing yargs
+yargs.version('1.1.0')
+
+//Creating add command
+yargs.command({
+    command : 'add',
+    describe : 'Add a new note',
+    handler : function(){
+        console.log('Adding a new note')
+    }
+})
+
+//Creating Remove command
+yargs.command({
+    command : 'remove',
+    describe :'Removing a Note',
+    handler : function(){
+        console.log('Removing a note')
+    }
+})
+
+//Creating Remove command
+yargs.command({
+    command : 'list',
+    describe :'Listing all Note',
+    handler : function(){
+        console.log('Listing all note')
+    }
+})
+
+//Creating Remove command
+yargs.command({
+    command : 'read',
+    describe :'Reading a Note',
+    handler : function(){
+        console.log('Reading all notes')
+    }
+})
+console.log(yargs.argv)
+// if(command === 'add')
+// {
+//     console.log('Adding Note')
+// }
+
+// else if(command === 'remove')
+// {
+//     console.log('Remove Note')
+// }
+
 // const validator = require('validator')
 // const { default: isEmail } = require('validator/lib/isemail.js')
 
-const msg = getNotes()
-console.log(msg)
-const greenMSG = chalk.red.inverse.bold('Success!')
-console.log(greenMSG)
+// const msg = getNotes()
+// console.log(msg)
+// const greenMSG = chalk.red.inverse.bold('Success!')
+// console.log(greenMSG)
+
+// console.log(process.argv[2])
 // console.log(validator.isURL ('saurin'))
 
 
