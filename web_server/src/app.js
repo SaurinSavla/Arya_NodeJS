f6714c76293f05f59cc2743d118862d9b843734b
SaurinSavla <saurin.savla@gmail.com>
2022-11-14 00:18:57 +0530

    sec 7 vid 46 serving up css,js and images

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,22 +1,29 @@
+const path = require('path')
 const express = require ('express')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname,'../public'))
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+const publicDirectoryPath = path.join(__dirname,'../public')
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name : 'Saurin'
-    },{
-        name : 'Devarya'
-    }])
-})
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
 
-app.get('/about', (req, res) => {
-    res.send('<h2>About Page</h2>')
-})
+// app.get('/help', (req, res) => {
+//     res.send([{
+//         name : 'Saurin'
+//     },{
+//         name : 'Devarya'
+//     }])
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h2>About Page</h2>')
+// })
 
 app.get('/weather', (req, res) => {
     res.send({
